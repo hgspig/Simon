b3d3308179b269e24a1364afdf97b2bd404fea18
hgspig <42699788+hgspig@users.noreply.github.com>
2023-03-06 08:28:04 -0700

    adding assets and more JS

--- scores.js
+++ scores.js
@@ -3,9 +3,10 @@ function loadScores() {
     const scoresText = localStorage.getItem('scores');
     if (scoresText) {
         scores = JSON.parse(scoresText);
-    }
+    } //not sure I understand what this is doing
 
     const tableBodyEl = document.querySelector('#scores');
+
     if (scores.length) {
         for (const [i, score] of scores.entries()){
             const positionTdEL = document.createElement('td');
@@ -15,8 +16,18 @@ function loadScores() {
 
             positionTdEL.textContent = i + 1;
             nameTdEL.textContent = score.name;
-            nameTdEL
+            scoreTdEL.textContent = score.score;
+            dateTdEl.textContent = score.date;
+
+            const rowEL = document.createElement('tr');
+            rowEL.appendChild(positionTdEL);
+            rowEL.appendChild(nameTdEL);
+            rowEL.appendChild(scoreTdEl);
+
+            tableBodyEl.appendChild(rowEL);
         }
+    } else {
+        tableBodyEl.innerHTML = '<tr><td colSpan = 4> Be the first to score</td></tr>'
     }
 }
 
